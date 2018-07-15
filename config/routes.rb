7df9b913ed328ed30c46bf432aede9f5169b2227
masato hojo <giraffe22ice@gmail.com>
2018-07-15 04:29:09 +0000

    test

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+    root to: 'toppages#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
+    get 'signup', to: 'users#new'
+    get 'rankings/want', to: 'rankings#want'
+    get 'rankings/have', to: 'rankings#have'
+    resources :users, only: [:show, :new, :create]
+    
+    resources :items, only: [:show, :new]
+    
+    resources :ownerships, only: [:create, :destroy]
+end
